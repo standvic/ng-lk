85cebb801041e26d7641830c8c8abeb29d70c950
standvic <standvic@gmail.com>
2019-12-29 21:13:35 +0300

    commit

--- temp/lastBackup/app/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { SignInComponent } from './sign-in/sign-in.component';
-export { SignOutComponent } from './sign-out/sign-out.component';
-export { MainComponent } from './main/main.component';
-//export { NavMenuComponent } from './nav-menu/nav-menu.component';
-export { MoneyDynamicBalanceComponent } from './money-dynamic-balance/money-dynamic-balance.component';
-
-
